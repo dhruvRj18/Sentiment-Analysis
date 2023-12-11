82520ffdbbbb452249198e3eb51d8f357c01badd
Dhruv Parmar <30573895+dhruvRj18@users.noreply.github.com>
2023-12-11 10:38:36 -0500

    feat: deployment done with xgb (#10)

--- app.py
+++ app.py
@@ -1,38 +1,82 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 import joblib
-import logging
-import json
+import numpy as np
+from gensim.models.doc2vec import Doc2Vec
+import demoji
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+demoji.download_codes()
+nltk.download('stopwords')
+nltk.download('punkt')
 
 app = Flask(__name__)
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)  # Set the logging level to DEBUG
-
-model = joblib.load('./data/xgboost_model.pkl')
+doc2vec_model = Doc2Vec.load('./data/doc2vec_model.pkl')
+xgboost_model = joblib.load('./data/xgboost_model.pkl')
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        data = json.loads(request.data.decode('utf-8'))
-        logging.debug(f"Received request: {data}")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-        text_content = data.get('text_content').encode('utf-8')
-        logging.debug(f"Received request with text_content: {text_content}")
 
-        if not text_content:
-            raise ValueError("Missing or empty 'text_content' in the request.")
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    if request.method == 'POST':
+        text_content = request.form['text_content']
 
-        prediction = model.predict([text_content])
-        readable_prediction = "positive" if prediction[0] == 1 else "negative"
+        if text_content:
+            text_vector = infer_doc2vec_vector(text_content)
+            sentiment_label = predict_sentiment(text_vector)
 
-        response_data = {'prediction': readable_prediction}
-        logging.debug(f"Predicted result: {response_data}")
+            return render_template('result.html', sentiment=sentiment_label)
 
-        return jsonify(response_data)
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        return jsonify({'error': str(e)}), 400
+    return render_template('index.html')
+
+
+def preprocess_text(text):
+    text = text.lower()
+
+    text = re.sub(r'https?:\/\/\S+', '', text)
+    text = re.sub(r'@[A-Za-z0-9]+', '', text)
+    text = re.sub(r'[^A-Za-z0-9\s]+', '', text)
+    text = demoji.replace(text, '')
+
+    tokens = word_tokenize(text)
+
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token not in stop_words]
+
+    preprocessed_text = ' '.join(tokens)
+
+    return preprocessed_text
+
+
+def infer_doc2vec_vector(text_content):
+    words = text_content.split()
+    return doc2vec_model.infer_vector(words)
+
+
+def decode_sentiment(sentiment_label):
+    if sentiment_label == 0:
+        return "Negative"
+    elif sentiment_label == 1:
+        return "Neutral"
+    elif sentiment_label == 2:
+        return "Positive"
+    else:
+        return "Unknown"
+
+
+def predict_sentiment(text_vector):
+    text_vector = np.array(text_vector).reshape(1, -1)
+    sentiment_label = xgboost_model.predict(text_vector)[0]
+
+    return decode_sentiment(sentiment_label)
+
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True)
